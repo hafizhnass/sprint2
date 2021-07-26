bae54d595c72c8221d9ffd621130fd58ebbaff89
hafizhnass <87346700+hafizhnass@users.noreply.github.com>
2021-07-26 14:37:58 +0700

    Add files via upload

---
+++ rmspanjang.js
@@ -0,0 +1,26 @@
+// function hitungLuasPersegiPanjang(panjang,lebar){
+//     luas = panjang * lebar
+
+//     document.write("Panjang : " + panjang + "<br/>");
+//     document.write("Lebar : " + lebar + "<br><br/>");
+//     document.write("Luas Persegi Panjang : " + luas)
+// }
+
+// panjang = prompt("Masukan Panjang");
+// lebar = prompt("Masukan Lebar");
+
+// hitungLuasPersegiPanjang(panjang, lebar)
+
+
+function hitungluaspersegipanjang(panjang, lebar){
+luas = panjang * lebar
+
+document.write("panjang:" + panjang + "<br/>")
+document.write("lebar:" + lebar + "<br/>")
+document.write("luas persegi panjang: " + luas)
+}
+
+panjang = prompt("input panjang")
+lebar = prompt("input lebar")
+
+hitungluaspersegipanjang(panjang, lebar)
