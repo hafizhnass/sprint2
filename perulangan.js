9c8a3ff5d1940d019d8cce26a04b9175d205455e
hafizhnass <87346700+hafizhnass@users.noreply.github.com>
2021-07-27 21:03:21 +0700

    Add files via upload

---
+++ perulangan.js
@@ -0,0 +1,7 @@
+// ['Wandi','Aldi','Acep','Putra','Hafif','Syofyan',"Rahmad"] 
+
+var namaSantri = ["wandi", "Aldi", "Acep", "Putra", "Hafif", "Sofyan", "RAhmad"]
+
+for(var i = 0; i < namaSantri.length; i++){
+    console.log(i + "," + "" + namaSantri[i]) 
+}
